a98138db3c89b38893512d2077f4f5f5cbf2009f
Dimitar Zurnov <mitkozurnov@gmail.com>
2024-04-18 19:23:38 +0300

    Update Cors origins

--- src/main/java/com/zurnov/bitcoin/insights/config/WebConfig.java
+++ src/main/java/com/zurnov/bitcoin/insights/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.zurnov.bitcoin.insights.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,8 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${RPC_ADDRESS}")
+    private String rpcAddress;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://" + rpcAddress + ":4200", "https://" + rpcAddress + ":4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
