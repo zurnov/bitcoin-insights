3b02a9627ba22e18f294d33fa1777b3c48a37528
zurnov <mitkozurnov@gmail.com>
2024-05-07 19:35:53 +0300

    Introduce openapi springdoc API documentation

--- src/main/java/com/zurnov/bitcoin/insights/controller/BlockchainNetworkController.java
+++ src/main/java/com/zurnov/bitcoin/insights/controller/BlockchainNetworkController.java
@@ -11,8 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BlockchainNetworkController {
 
     BlockchainBlockService blockchainBlockService;
