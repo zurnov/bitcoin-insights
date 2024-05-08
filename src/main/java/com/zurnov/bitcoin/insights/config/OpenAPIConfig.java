ab567ec330473800d3006938ff5d722a9e56b598
zurnov <mitkozurnov@gmail.com>
2024-05-08 19:46:59 +0300

    Add server url to both dev and prod spring profiles for swagger config

--- src/main/java/com/zurnov/bitcoin/insights/config/OpenAPIConfig.java
+++ src/main/java/com/zurnov/bitcoin/insights/config/OpenAPIConfig.java
@@ -3,14 +3,24 @@ package com.zurnov.bitcoin.insights.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
 
+    @Value("${openapi.server.url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
+
+        Server server = new Server();
+        server.setUrl(serverUrl);
 
         Contact contact = new Contact();
         contact.setUrl("https://zurnov.com");
@@ -23,7 +33,7 @@ public class OpenAPIConfig {
         info.setVersion("1.0.0");
         info.setDescription("Bitcoin blockchain explorer");
 
-        return new OpenAPI().info(info);
+        return new OpenAPI().info(info).servers(List.of(server));
     }
 
 }
