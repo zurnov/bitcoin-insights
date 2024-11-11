6329505170b81737a2aa1720547dd6fdae9acb51
Dimitar Zurnov <55513656+zurnov@users.noreply.github.com>
2024-11-11 23:19:22 +0200

    Enable Spring Scheduling

--- src/main/java/com/zurnov/bitcoin/insights/Application.java
+++ src/main/java/com/zurnov/bitcoin/insights/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
