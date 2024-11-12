712f6d90bfda04d6b62da8fb5bdd90e2ec51797b
Dimitar Zurnov <55513656+zurnov@users.noreply.github.com>
2024-11-12 10:13:54 +0200

    Fix the imports for the Application

--- src/main/java/com/zurnov/bitcoin/insights/Application.java
+++ src/main/java/com/zurnov/bitcoin/insights/Application.java
@@ -2,8 +2,7 @@ package com.zurnov.bitcoin.insights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
