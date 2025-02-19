89e7daee8959dcae9b2bf1ccf43d448159f13508
zurnov <mitkozurnov@gmail.com>
2025-02-19 22:01:37 +0200

    Update CORS and swagger dev and prod address

--- src/main/java/com/zurnov/bitcoin/insights/config/WebConfig.java
+++ src/main/java/com/zurnov/bitcoin/insights/config/WebConfig.java
@@ -3,11 +3,9 @@ package com.zurnov.bitcoin.insights.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${RPC_ADDRESS}")
@@ -16,20 +14,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins(
-//                        "http://" + rpcAddress + ":4200",
-//                        "https://" + rpcAddress + ":4200",
-//                        "http://localhost:4200",
-//                        "http://localhost:8000",
-//                        "https://api.zurnov.com",
-//                        "https://dev.zurnov.com",
-//                        "https://zurnov.com",
-//                        "https://www.zurnov.com",
-//                        "https://explore21.com",
-//                        "https://www.explore21.com",
-//                        "https://dev.explore21.com",
-//                        "https://api.explore21.com")
-                .allowedOrigins("*")
+                .allowedOrigins(
+                        "http://" + rpcAddress + ":4200",
+                        "https://" + rpcAddress + ":4200",
+                        "http://localhost:4200",
+                        "http://localhost:8000",
+                        "https://api.zurnov.com",
+                        "https://dev.zurnov.com",
+                        "https://explore21.com",
+                        "https://www.explore21.com",
+                        "https://dev.explore21.com",
+                        "https://api.explore21.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
